72591d9ae3e3444ad403946070db0881c29954a8
Amit <amit.aks2099@gmail.com>
2021-11-08 19:38:16 +0530

    second commit

--- src/main/java/com/example/githubjenkinsdocker/GithubJenkinsDockerApplication.java
+++ src/main/java/com/example/githubjenkinsdocker/GithubJenkinsDockerApplication.java
@@ -22,6 +22,6 @@ public class GithubJenkinsDockerApplication {
 	
 	@GetMapping
 	public String testMethod() {
-		return "Integration test of Github-Jenkins-Docker...!"; 
+		return "Integration test of Github-Jenkins-Docker..!"; 
 	}
 }
